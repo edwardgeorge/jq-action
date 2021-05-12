fedd417c0020c7fee24e954318e4ac54f3af7af3
Edward George <edwardgeorge@gmail.com>
2021-05-12 14:20:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# JQ Action
+
+Process a value with a jq script and output to a step output.
+
+## Inputs
+
+### `input`
+
+**Required** The input value to be processed by script.
+
+### `script`
+
+**Required** The JQ script.
+
+### `compact`
+
+Set to `"true"` to emit compact output from jq.
+
+### `raw-output`
+
+Set to `"true"` to have jq emit raw strings.
+
+## Outputs
+
+### `output`
+
+The output of the JQ script.
+
+## Example usage
+
+```yaml
+uses: edwardgeorge/jq-action@main
+with:
+  input: "[{\"num\": 1}, {\"num\": 2}]"
+  script: "[.[] | select(.num == '1') | .num]"
+```
---
+++ index.js
@@ -0,0 +1,77 @@
+const core = require('@actions/core');
+const io = require('@actions/io');
+const tc = require('@actions/tool-cache');
+const ex = require('@actions/exec');
+
+//async function ensureBinDir() {
+//  bindir = `${process.env.HOME}/.local/bin`
+//  if (!fs.existsSync(bindir)) {
+//    await io.mkdirP(bindir);
+//    core.addPath(bindir);
+//  }
+//  if(!fs.statSync(bindir).isDirectory()) {
+//    throw {"message": `${bindir} exists and is not directory`};
+//  }
+//  return bindir;
+//}
+//
+//async function setup_jq(bindir) {
+//  var jqPath = await io.which('jq', false);
+//  if (! jqPath) {
+//    const bindl = await tc.find("jq", "1.6")
+//    const bindl = await tc.downloadTool("https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64");
+//    const st = fs.statSync(bindl);
+//    fs.chmodSync(bindl, st.mode | fs.constants.S_IXUSR | fs.constants.S_IXGRP | fs.constants.S_IXOTH);
+//    await tc.cacheFile(bindl, 'jq', 'jq', '1.6');
+//    jqPath = path.join([bindir, 'jq']);
+//    await io.cp(bindl, jqPath);
+//    if(!await io.which('jq', false)) {
+//      core.addPath(bindir);
+//    }
+//  }
+//}
+
+function MissingBinary(program) {
+  this.message = `${program} not installed on path`;
+}
+
+async function findBinary() {
+  let binpath = await io.which('jq', false);
+  if (!binpath) {
+    binpath = await tc.find('jq', '*');
+    if(!binpath) {
+      throw new MissingBinary('jq');
+    }
+  }
+  return binpath;
+}
+
+async function run() {
+  try {
+    var output = '';
+    const jq = await findBinary();
+    const input = core.getInput("input", {required: true});
+    const script = core.getInput("script", {required: true});
+    const flags = [];
+    if (core.getInput("compact") == 'true') {
+      flags.push('c');
+    }
+    if (core.getInput("raw-output") == 'true') {
+      flags.push('r');
+    }
+    const args = flags.length > 0 ? [`-${flags.join('')}`, script] : [script];
+    await ex.exec(jq, args, {
+      listeners: {
+        stdout: (data) => {
+          output += data.toString();
+        }
+      },
+      input: Buffer.from(input, 'utf8')
+    });
+    core.setOutput('output', output);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
